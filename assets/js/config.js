3e6111511c43126bc0eaa3d11bd3cf09edecff51
TEkdahl92 <106709941+TEkdahl92@users.noreply.github.com>
2022-07-07 19:08:26 -0500

    save changes

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,2 +1,2 @@
-const MY_API_KEY = 'a0eee688dcmshf0cd55b72775dafp1f835ajsn7ed18ac2d93b';
-export { MY_API_KEY }
+const _MY_API_KEY = 'a0eee688dcmshf0cd55b72775dafp1f835ajsn7ed18ac2d93b';
+export { _MY_API_KEY }
--- assets/js/japan.js
+++ assets/js/japan.js
@@ -1,11 +1,4 @@
-import { MY_API_KEY } from './config.js'
-
-fetch('https://hotels4.p.rapidapi.com/locations/v2/search?query=new%20york&locale=en_US&currency=USD',)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
-
-  goBack.addEventListener("click", function () {
+goBack.addEventListener("click", function () {
 	window.location.replace("./index.html");
 });
 
@@ -19,3 +12,10 @@ function initMap() {
 }
 
 window.initMap = initMap;
+
+import { _MY_API_KEY } from './config.js';
+
+fetch('https://hotels4.p.rapidapi.com/locations/v2/search?query=Japan&locale=en_US&currency=USD', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
