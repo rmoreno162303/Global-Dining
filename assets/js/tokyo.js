dabcab4eefd4b9e28c1a3d0b6a5c7db8fd015530
TEkdahl92 <106709941+TEkdahl92@users.noreply.github.com>
2022-07-11 13:44:04 -0500

    save changes

--- assets/js/tokyo.js
+++ assets/js/tokyo.js
@@ -14,3 +14,16 @@ window.initMap = initMap;
 goBack.addEventListener("click", function () {
   window.location.replace("../../index.html");
 });
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a0eee688dcmshf0cd55b72775dafp1f835ajsn7ed18ac2d93b',
+		'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
+	}
+};
+
+fetch('https://hotels4.p.rapidapi.com/locations/v2/search?query=tokyo&locale=en_US&currency=', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
