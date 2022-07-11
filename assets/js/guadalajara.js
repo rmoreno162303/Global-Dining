de1b220b1afcc7e33a5189a53c77767674d6c7e4
rmoreno162303 <105377377+rmoreno162303@users.noreply.github.com>
2022-07-11 13:54:51 -0500

    updating code for api

--- assets/js/guadalajara.js
+++ assets/js/guadalajara.js
@@ -6,24 +6,15 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-<<<<<<< HEAD:assets/js/japan.js
-    center: { lat: 35.6762, lng: 139.770 },
-    zoom: 10,
-=======
     center: { lat: 20.65, lng: -103.34 },
     zoom: 11,
->>>>>>> 58fd50eff8f96115f12ad865af10de595a6b6de3:assets/js/guadalajara.js
   });
 }
 
 window.initMap = initMap;
 
 goBack.addEventListener("click", function () {
-<<<<<<< HEAD:assets/js/japan.js
-  window.location.replace("../../index.html");
-=======
 	window.location.replace("../../index.html");
->>>>>>> 58fd50eff8f96115f12ad865af10de595a6b6de3:assets/js/guadalajara.js
 });
 
 const options = {
